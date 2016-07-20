d22aa6115a041c4b856534bc56a8c4eb7f4b4f1d
AntonPavl <pavlenok.anton@gmail.com>
2016-07-20 17:53:18 +0300

    Queue

--- Day10/YieldcollectionNTest/QueueTests.cs
+++ Day10/YieldcollectionNTest/QueueTests.cs
@@ -8,8 +8,9 @@ namespace YieldcollectionNTest
     [TestFixture]
     public class QueueTests
     {
+
         [Test]
-        public void QueueTest()
+        public void QueueTest_foreach_test()
         {
             var queue = new Queue<int>();
             var array = new int[30];
@@ -20,15 +21,51 @@ namespace YieldcollectionNTest
                 queue.Enqueue(temp);
                 array[i] = temp; 
             }
-
-            bool res = true;
-            int index = 0;
+            int index = -1;
             foreach ( var e in queue)
             {
-                if (array[index] != e) res = false;
-                index++;
+                Assert.AreEqual(array[++index], e);
             }
-            Assert.AreEqual(res, true);
+        }
+
+        [Test]
+        public void QueueTest_Dequeue_Enqueue_Test()
+        {
+            var queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Dequeue();
+            queue.Enqueue(3);
+            Assert.AreEqual(queue.Dequeue(), 2);
+            Assert.AreEqual(queue.Dequeue(), 3);
+        }
+
+        [Test]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void QueueTest_Dequeue_exception()
+        {
+            Queue<int> q = new Queue<int>();
+            q.Dequeue();
+        }
+
+        [Test]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void QueueTest_Peek_exception()
+        {
+            Queue<int> q = new Queue<int>();
+            q.Peek();
+        }
+
+        [Test]
+        public void QueueTest_EnqueueStringFromAnotherStringcllection()
+        {
+            string[] temp = { "1", "2", "3", "4" };
+            Queue<string> q = new Queue<string>(temp);
+            int index = -1;
+            foreach (var item in q)
+            {
+                Assert.AreEqual(item, temp[++index]);
+            }
         }
     }
 }
