86f1cc8ed7f96b4a9b551adcb5712840dd3b8007
AntonPavl <pavlenok.anton@gmail.com>
2016-07-20 17:00:36 +0300

    Temp 1-2

---
+++ Day10/FibonachiNTest/FibonachiTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using FibonachiDLL;
+using System.Diagnostics;
+
+namespace FibonachiNTest
+{
+    [TestFixture]
+    public class FibonachiTest
+    {
+        [Test]
+        public void Evaluate()
+        {
+
+            Debug.WriteLine(Fibonachi.Evaluate(5));
+            Debug.Write(Fibonachi.GetFibonachiNumber(8));
+        }
+    }
+}
---
+++ Day10/YieldcollectionNTest/QueueTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+using YieldCollection;
+using System.Diagnostics;
+
+namespace YieldcollectionNTest
+{
+    [TestFixture]
+    public class QueueTests
+    {
+        [Test]
+        public void QueueTest()
+        {
+            var queue = new Queue<int>();
+            var array = new int[30];
+            Random rand = new Random();
+            for (int i = 0; i < 30; i++)
+            {
+                int temp = rand.Next(100);
+                queue.Enqueue(temp);
+                array[i] = temp; 
+            }
+
+            bool res = true;
+            int index = 0;
+            foreach ( var e in queue)
+            {
+                if (array[index] != e) res = false;
+                index++;
+            }
+            Assert.AreEqual(res, true);
+        }
+    }
+}
