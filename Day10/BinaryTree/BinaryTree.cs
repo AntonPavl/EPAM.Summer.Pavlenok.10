5bac8dc6af0c3490b8f47d4e454a87cc0ce29802
AntonPavl <pavlenok.anton@gmail.com>
2016-07-24 15:57:49 +0300

    TempTree

---
+++ Day10/BinaryTree/BinaryTree.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    public class BinaryTree<T> : ICollection<T>,IEquatable<BinaryTree<T>>
+    {
+        private class Node<T>
+        {
+            public T Value{ get; set;}
+            public Node<T> Left{ get; set;}
+            public Node<T> Right{  get; set;}
+            public Node(T value)
+            {
+                Value = value;
+            }
+        }
+
+        public T RootLeftForTest()
+        {
+            return root.Left.Value;
+        }
+        public T RootRightForTest()
+        {
+            return root.Left.Value;
+        }
+
+        private Node<T> root;
+        private IComparer<T> comparer;
+        public BinaryTree() : this(Comparer<T>.Default){}
+        public BinaryTree(IEnumerable<T> collection) : this(collection, Comparer<T>.Default){}
+        public BinaryTree(IComparer<T> comparer)
+        {
+            if (ReferenceEquals(comparer,null)) this.comparer = Comparer<T>.Default;
+            this.comparer = comparer;
+        }
+        public BinaryTree(IEnumerable<T> collection, IComparer<T> comparer) : this(comparer)
+        {
+            AddRange(collection);
+        }
+        /// <summary>
+        /// Get MinValue
+        /// </summary>
+        public T MinValue => SomeValue((t) => t.Left);
+        /// <summary>
+        /// Get MaxValue
+        /// </summary>
+        public T MaxValue => SomeValue((t) => t.Right);
+
+        private T SomeValue(Func<Node<T>,Node<T>> fun)
+        {
+            if (ReferenceEquals(root,null)) throw new InvalidOperationException();
+            Node<T> current = root;
+            while (fun(current) != null)
+                current = fun(current);
+            return current.Value;
+        }
+
+        /// <summary>
+        /// Add collection to tree
+        /// </summary>
+        /// <param name="collection"></param>
+        public void AddRange(IEnumerable<T> collection)
+        {
+            foreach (var item in collection)
+                Add(item);
+        }
+
+        /// <summary>
+        /// Get number of elements
+        /// </summary>
+        public int Count
+        {
+            get;
+            private set;
+        }
+        /// <summary>
+        /// Add Item to tree
+        /// </summary>
+        /// <param name="item"></param>
+        public void Add(T item)
+        {
+            var node = new Node<T>(item);
+
+            if (root == null)
+                root = node;
+            else
+            {
+                Node<T> current = root;
+                Node<T> parent = null;
+
+                while (current != null)
+                {
+                    parent = current;
+                    if (comparer.Compare(item, current.Value) < 0)
+                        current = current.Left;
+                    else
+                        current = current.Right;
+                }
+
+                if (comparer.Compare(item, parent.Value) < 0)
+                    parent.Left = node;
+                else
+                    parent.Right = node;
+            }
+            ++Count;
+        }
+        /// <summary>
+        /// Remove item from tree
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public bool Remove(T item)
+        {
+            if (root == null)
+                return false;
+
+            Node<T> current = root;
+            Node<T> parent = null;
+
+            int result;
+            do
+            {
+                result = comparer.Compare(item, current.Value);
+                if (result < 0)
+                {
+                    parent = current;
+                    current = current.Left;
+                }
+                else if (result > 0)
+                {
+                    parent = current;
+                    current = current.Right;
+                }
+                if (current == null)
+                    return false;
+            }
+            while (result != 0);
+
+            if (current.Right == null)
+            {
+                if (current == root)
+                    root = current.Left;
+                else
+                {
+                    result = comparer.Compare(current.Value, parent.Value);
+                    if (result < 0)
+                        parent.Left = current.Left;
+                    else
+                        parent.Right = current.Left;
+                }
+            }
+            else if (current.Right.Left == null)
+            {
+                current.Right.Left = current.Left;
+                if (current == root)
+                    root = current.Right;
+                else
+                {
+                    result = comparer.Compare(current.Value, parent.Value);
+                    if (result < 0)
+                        parent.Left = current.Right;
+                    else
+                        parent.Right = current.Right;
+                }
+            }
+            else
+            {
+                Node<T> min = current.Right.Left, prev = current.Right;
+                while (min.Left != null)
+                {
+                    prev = min;
+                    min = min.Left;
+                }
+                prev.Left = min.Right;
+                min.Left = current.Left;
+                min.Right = current.Right;
+
+                if (current == root)
+                    root = min;
+                else
+                {
+                    result = comparer.Compare(current.Value, parent.Value);
+                    if (result < 0)
+                        parent.Left = min;
+                    else
+                        parent.Right = min;
+                }
+            }
+            --Count;
+            return true;
+        }
+        public void Clear()
+        {
+            root = null;
+            Count = 0;
+        }
+        /// <summary>
+        /// copy tree to array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="arrayIndex"></param>
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (ReferenceEquals(array, null)) throw new ArgumentNullException();
+            foreach (var value in this)
+                array[arrayIndex++] = value;
+        }
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// check 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public bool Contains(T item)
+        {
+            var current = root;
+            while (current != null)
+            {
+                var result = comparer.Compare(item, current.Value);
+                if (result == 0)
+                    return true;
+                if (result < 0)
+                    current = current.Left;
+                else
+                    current = current.Right;
+            }
+            return false;
+        }
+        private Queue<Node<T>> queue;
+        private void PreOrderTravers(Node<T> traver)
+        {
+            if (traver != null)
+            {
+                queue.Enqueue(traver);
+                PreOrderTravers(traver.Left);
+                PreOrderTravers(traver.Right);
+            }
+        }
+        private void InOrderTravers(Node<T> traver)
+        {
+            var ret = new Stack<Node<T>>();
+            if (traver != null)
+            {         
+                InOrderTravers(traver.Left);
+                queue.Enqueue(traver);
+                InOrderTravers(traver.Right);
+            }
+        }
+        private void PostOrderTravers(Node<T> traver)
+        {
+            if (traver != null)
+            {
+                PostOrderTravers(traver.Left);
+                PostOrderTravers(traver.Right);
+                queue.Enqueue(traver);
+            }
+        }
+        private IEnumerable<T> Order(Action<Node<T>> getQueue)
+        {
+            queue = new Queue<Node<T>>();
+            getQueue(root);
+            while( queue.Count > 0)
+            {
+                yield return queue.Dequeue().Value;
+            }
+        }
+        public IEnumerable<T> Inorder() => Order(InOrderTravers);
+        public IEnumerable<T> Preorder() => Order(PreOrderTravers);
+        public IEnumerable<T> Postorder() => Order(PostOrderTravers);
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return Preorder().GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public bool Equals(BinaryTree<T> other)
+        {
+            return this.Equals(other);
+        }
+    }
+}
---
+++ Day10/BinaryTreeTest/BinaryTreeTest.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using NUnit.Framework;
+using Tree;
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BinaryTreeTest
+{
+    [TestFixture]
+    public class BinaryTreeTest
+    {
+        [Test]
+        public void Max_Min_Value()
+        {
+            BinaryTree<int> bt = new BinaryTree<int>();
+            Random rand = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                bt.Add(i);
+            }
+            Assert.AreEqual(0, bt.MinValue);
+            Assert.AreEqual(19, bt.MaxValue);
+        }
+
+        [Test]
+        public void PreOrder()
+        {
+            BinaryTree<int> bt = new BinaryTree<int>();
+            Random rand = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                bt.Add(i);
+            }
+            int test = 0;
+            foreach (var item in bt.Preorder())
+            {
+                Assert.AreEqual(test, item);
+                test++;
+            }
+        }
+        [Test]
+        public void PostOrder()
+        {
+            BinaryTree<int> bt = new BinaryTree<int>();
+            Random rand = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                bt.Add(i);
+            }
+            int test = 4;
+            foreach (var item in bt.Postorder())
+            {
+                Assert.AreEqual(test, item);
+                test--;
+            }
+        }
+
+        [Test]
+        public void InOrder()
+        {
+            BinaryTree<int> bt = new BinaryTree<int>();
+            Random rand = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                bt.Add(i);
+            }
+            int test = 0;
+            foreach (var item in bt.Inorder())
+            {
+                Assert.AreEqual(test, item);
+                test++;
+            }
+        }
+
+
+
+        private class LeftcomparatorInt : IComparer<int>
+        {
+            public int Compare(int x, int y)
+            {
+                return y.CompareTo(x);
+            }
+        }
+        [Test]
+        public void AllLeftTreeInt()
+        {
+            BinaryTree<int> bt = new BinaryTree<int>(new LeftcomparatorInt());
+            Random rand = new Random();
+            List<int> list = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                int value = rand.Next(100);
+                bt.Add(value);
+                list.Add(value);
+            }
+            list.Sort(new LeftcomparatorInt());
+            CollectionAssert.AreEqual(bt.Inorder(), list);
+        }
+
+
+
+        private class LeftcomparatorString : IComparer<string>
+        {
+            public int Compare(string x, string y)
+            {
+                return y.CompareTo(x);
+            }
+        }
+
+        [Test]
+        public void AllLeftTreeString()
+        {
+            BinaryTree<string> bt = new BinaryTree<string>(new LeftcomparatorString());
+            Random rand = new Random();
+            List<string> list = new List<string>();
+            for (int i = 0; i < 10; i++)
+            {
+                string value = rand.Next(100).ToString();
+                bt.Add(value);
+                list.Add(value);
+            }
+            list.Sort(new LeftcomparatorString());
+            CollectionAssert.AreEqual(bt.Inorder(), list);
+        }
+
+
+        public struct Point
+        {
+            public int x;
+            public int y;
+            public Point(int x,int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+        private class LeftcomparatorPoint : IComparer<Point>
+        {
+            public int Compare(Point x, Point y)
+            {
+                return (y.x + y.y).CompareTo(x.x + x.y);
+            }
+        }
+
+
+        [Test]
+        public void AllLeftTreePoint()
+        {
+            BinaryTree<Point> bt = new BinaryTree<Point>(new LeftcomparatorPoint());
+            Random rand = new Random();
+            List<Point> list = new List<Point>();
+            for (int i = 0; i < 10; i++)
+            {
+                int value = rand.Next(100);
+                int value2 = rand.Next(100);
+                Point p = new Point(value, value2);
+                bt.Add(p);
+                list.Add(p);
+            }
+            list.Sort(new LeftcomparatorPoint());
+            CollectionAssert.AreEqual(bt.Inorder(), list);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TreePoint()
+        {
+            BinaryTree<Point> bt = new BinaryTree<Point>();
+            Random rand = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                int value = rand.Next(100);
+                int value2 = rand.Next(100);
+                Point p = new Point(value, value2);
+                bt.Add(p);
+            }
+            foreach (var item in bt.Inorder())
+            {
+                Debug.WriteLine(item);
+            }
+        }
+    }
+}
