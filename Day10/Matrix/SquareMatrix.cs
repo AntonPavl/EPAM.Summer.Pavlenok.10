0aac1936f5c5d7f7de273e7c4c3adff4a279ba7d
AntonPavl <pavlenok.anton@gmail.com>
2016-07-31 13:46:50 +0300

    Matrix

---
+++ Day10/Matrix/SquareMatrix.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrixs
+{
+    public class SquareMatrix<T> : AbstractMatrix<T>
+    {
+        public SquareMatrix(int size = 2)
+        {
+            array = new T[size * size];
+            Size = size;
+        }
+        public SquareMatrix(IEnumerable<T> collection)
+        {
+            double temp = Math.Sqrt(collection.Count());
+            temp = temp - Math.Truncate(temp);
+            if (temp > 0) throw new ArgumentException();
+            Size = (int)temp * (int)temp;
+            array = new T[Size];
+            int index = 0;
+            foreach (var item in collection)
+            {
+                array[index++] = item;
+            }
+        }
+
+        protected override T GetValue(int i, int j)
+        {
+            return array[i * Size + j];
+        }
+
+        protected override void SetValue(int i, int j, T value)
+        {
+            array[i * Size + j] = value;
+        }
+    }
+}
